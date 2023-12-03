ad6dc04ac9a2c9001c5a2a1b9761b81315ea1a14
thanhngodinh <thanh.ngodinh2000@gmail.com>
2023-12-03 22:14:47 +0700

    git init

---
+++ src/layouts/dashboard/config-navigation.jsx
@@ -0,0 +1,27 @@
+import SvgColor from 'src/components/svg-color';
+
+// ----------------------------------------------------------------------
+
+const icon = (name) => (
+  <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />
+);
+
+const navConfig = [
+  {
+    title: 'dashboard',
+    path: '/admin/dashboard',
+    icon: icon('ic_analytics'),
+  },
+  {
+    title: 'user',
+    path: '/admin/user',
+    icon: icon('ic_user'),
+  },
+  {
+    title: 'blog',
+    path: '/admin/blog',
+    icon: icon('ic_blog'),
+  },
+];
+
+export default navConfig;
---
+++ src/routes/sections.jsx
@@ -0,0 +1,48 @@
+import { lazy, Suspense } from 'react';
+import { Outlet, Navigate, useRoutes } from 'react-router-dom';
+
+import DashboardLayout from 'src/layouts/dashboard';
+
+export const IndexPage = lazy(() => import('src/pages/app'));
+export const BlogPage = lazy(() => import('src/pages/blog'));
+export const UserPage = lazy(() => import('src/pages/user'));
+export const LoginPage = lazy(() => import('src/pages/login'));
+export const ProductsPage = lazy(() => import('src/pages/products'));
+export const Page404 = lazy(() => import('src/pages/page-not-found'));
+
+// ----------------------------------------------------------------------
+
+export default function Router() {
+  const routes = useRoutes([
+    {
+      path: 'admin',
+      element: (
+        <DashboardLayout>
+          <Suspense>
+            <Outlet />
+          </Suspense>
+        </DashboardLayout>
+      ),
+      children: [
+        { element: <IndexPage />, path: 'dashboard'},
+        { path: 'user', element: <UserPage /> },
+        { path: 'blog', element: <BlogPage /> },
+      ],
+    },
+    {
+      path: '',
+      element: <LoginPage />,
+      index: true
+    },
+    {
+      path: '404',
+      element: <Page404 />,
+    },
+    {
+      path: '*',
+      element: <Navigate to="/404" replace />,
+    },
+  ]);
+
+  return routes;
+}
